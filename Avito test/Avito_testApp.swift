c67d8684bab713f9926633b1fe50808625e82d91
Ruslan Akhmetgareev <firemann33@gmail.com>
2024-03-04 15:58:14 +0500

    Initial Commit

---
+++ Avito test/Avito_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avito_testApp.swift
+//  Avito test
+//
+//  Created by Руслан on 4.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Avito_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
